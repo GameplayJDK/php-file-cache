339d6c99b433078a321152a36e1ab22982a519c0
GameplayJDK <github@gameplayjdk.de>
2019-12-19 00:43:01 +0100

    Add example and rename package

--- example/example.php
+++ example/example.php
@@ -1,19 +1,23 @@
-# php-file-cache
-
-Low-level, file based cache using `var_export()` and some magic. Optionally [PSR-16](https://www.php-fig.org/psr/psr-16/) compliant.
-
-## Installation
-
-```bash
-composer require gameplayjdk/php-file-cache
-```
-
-## Usage
-
-```php
 <?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2019 GameplayJDK
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-require __DIR__ . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Cache\Cache;
 use Cache\Psr16\Cache as Psr16Cache;
@@ -21,7 +25,7 @@ use Cache\Psr16\Cache as Psr16Cache;
 $key = 'an_example_key';
 
 // Create cache object.
-$cache = new Cache(__DIR__ .  '/example/cache');
+$cache = new Cache(__DIR__ .  '/cache');
 
 // Set something to the cache.
 $value = 'some string value';
@@ -121,9 +125,3 @@ echo PHP_EOL;
 
 // The PSR-16 compliant cache also supports sanitizing keys before using them.
 // It is generally advised to use the adapter class.
-
-```
-
-## License
-
-It's MIT.
