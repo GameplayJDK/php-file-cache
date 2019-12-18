359e3c2956d623b67510348bd18b47104dae34c2
GameplayJDK <github@gameplayjdk.de>
2019-12-18 20:51:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# php-cache
+
+Low-level, file based cache using `var_export()` and some magic. Optionally [PSR-16](https://www.php-fig.org/psr/psr-16/) compliant.
---
+++ src/Help/Directory.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2019 GameplayJDK
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Cache\Help;
+
+/**
+ * Class Directory
+ *
+ * @package Cache\Help
+ */
+final class Directory
+{
+    private function __construct()
+    {
+    }
+
+    /**
+     * List content of a directory naively.
+     *
+     * @param string $path
+     * @return array|string[]
+     */
+    public static function list(string $path): array
+    {
+        $content = [];
+        $handle = @opendir($path);
+
+        if ($handle !== false) {
+            while (($entry = readdir($handle)) !== false) {
+                $content[] = $entry;
+            }
+
+            closedir($handle);
+        }
+
+        return $content;
+    }
+
+    /**
+     * List content of a directory naively. Filter entries not matching a given pattern.
+     *
+     * @param string $path
+     * @param string $pattern
+     * @return array
+     */
+    public static function listPattern(string $path, string $pattern = ''): array
+    {
+        $content = static::list($path);
+
+        if (empty($content) || empty($pattern)) {
+            return $content;
+        }
+
+        return array_filter($content, function (string $entry) use ($pattern) {
+            return preg_match($pattern, $entry);
+        });
+    }
+}
