b385f4ac2e431f85cf658727828e720ba227ebf9
GameplayJDK <github@gameplayjdk.de>
2020-01-04 17:26:02 +0100

    Update copyright

--- src/Cache.php
+++ src/Cache.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The MIT License (MIT)
- * Copyright (c) 2019 GameplayJDK
+ * Copyright (c) 2020 GameplayJDK
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
